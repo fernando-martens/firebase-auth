0ad1c3b1854fc62f8d3d0693505a4c911af96f92
Fernando Martens <fernando.martens@outlook.com>
2022-12-21 00:03:32 -0300

    botao de login index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
+import { Box, Flex, HStack } from "@chakra-ui/react";
 import React from "react";
 import Link from "../components/Link";
 
 const IndexPage = () => 
 	<>
-		<h1>Hello Next.js 👋</h1>
-		<Link href="./" name="Home" />
+	
+		<Flex bg="gray.100" align="center" justify="center" h="100vh">
+			<Box bg="white" p={6} rounded="md">
+				<HStack spacing={4} align="center">
+					<Link href="./" name="Home" />
+					<Link href="./login" name="Login" colorScheme="purple" />
+				</HStack>
+			</Box>
+		</Flex>
 	</>		
 ;
 
